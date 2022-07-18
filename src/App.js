a392b74fa778a0cf3c26d6b1e82acbfe5a3c73cd
chandrulogan <78639242+chandrulogan@users.noreply.github.com>
2022-07-18 15:39:55 +0530

    ZEN CLASS 21 Assignment

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Pricing from "./Pricing";
+import "./style.css"
+
+// const App = ()=>{
+//   return<></>;
+// };
+
+// export default App; // without export you cannot use this in or link this in other files
+
+// or 
+
+export default function App() {
+  return <div>
+    <Pricing />
+  </div>
+}
